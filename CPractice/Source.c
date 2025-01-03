5874ac77e1396d203bf019a20b997f261421de93
Bgarcia117 <bryant.garcia.1771@gmail.com>
2025-01-03 15:30:28 -0600

    Created method to check the capacity of my structure array. Created a method to modify the members of each structure.

--- CPractice/Source.c
+++ CPractice/Source.c
@@ -1,22 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
 #include <stdio.h>
+#include <stdlib.h>
 
 #define INITIAL_CAPACITY 5
+#define MAX_LENGTH 20
 
-struct Contact {
-	char* firstName;
-	char* lastName;
-	char* phoneNum;
-};
 
-struct Array {
+typedef struct {
+	char firstName[MAX_LENGTH];
+	char lastName[MAX_LENGTH];
+	char phoneNum[MAX_LENGTH];
+} Contact;
+
+typedef struct {
 	Contact* array;
-	size_t used; // Unisigned ints since we only need positive nums
-	size_t size;
-};
+	int used;
+	int size;
+} Array;
 
-void initArray(Array*, size_t);
-void insertInArray(Array*, int);
+void initArray(Array*, int);
+void checkArrayCapacity(Array*, int);
 void freeArray(Array*);
+void addContact(Array*);
 
 
 int main() {
@@ -24,59 +31,70 @@ int main() {
 	Array friends; // Instantiation of dynamic array structure
 	initArray(&friends, INITIAL_CAPACITY); // Initializes members of the structures
 
-	int selection = -1;
+	int selection = 0;
 
 	while (selection != 6) {
-		printf("Please choose from the following options: ");
-		printf("1) Add a contact");
-		printf("2) Delete a contact");
-		printf("3) Find a contact");
-		printf("4) Display contacts");
-		printf("5) Erase a contact");
-		printf("6) Close program");
+		printf("Please choose from the following options: \n");
+		printf("1) Add a contact\n");
+		printf("2) Delete a contact\n");
+		printf("3) Find a contact\n");
+		printf("4) Display contacts\n");
+		printf("5) Erase a contact\n");
+		printf("6) Close program\n");
+		printf("Please make a selection(1-6): \n");
+		scanf("%d", &selection);
+		getchar();
+
+		switch (selection) {
+			case 1: // Adds a contact
+				addContact(&friends);
+				printf("-----------------------------------------------\n");
+				printf((friends.array + friends.used)->firstName);
+				printf((friends.array + friends.used)->lastName);
+				printf((friends.array + friends.used)->phoneNum);
+				printf("-----------------------------------------------\n");
+
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			default:
+				printf("Please make a valid selection.");
+				scanf("%d", &selection);
+				getchar();
+		}
 	}
-
-	switch (selection) {
-	    case 1: // Adds a contact
-
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		default:
-			printf("Please make a valid selection.");
-	}
-
-
 
 }
 
-void initArray(Array* a, size_t initalSize) {
-	a->array = malloc(initialSize * sizezof(Contact));
+void initArray(Array* a, int initialSize) {
+	a->array = (Contact*)malloc(initialSize * sizeof(Contact));
 	a->used = 0;
 	a->size = initialSize;
 }
 
-void insertInArray(Array* a, int element) {
+void checkArrayCapacity(Array* a, int element) {
 	if (a->used == a->size) { // Checks if limit has been reached (used can go up to size)
 		a->size *= 2; // Doubles size
-		Contact* temp_array = realloc(a->array, a->size * sizeof(Contact)); // Reallocates space
-	}
+		Contact* temp_array = realloc(a->array, a->size * sizeof(Contact)); // Temp variable to check if reallocation is successful
 
-	if (temp_array == NULL) {
-		printf("Memory Allocation Failed!");
-		return;
+		if (temp_array == NULL) {
+			printf("Memory Allocation Failed!");
+			printf("Phone Book Full!");
+			printf("Please try again.");
+			return;
+		}
+		else {
+			a->array = temp_array; // Resizes array if memory allocation is successful
+		}
 	}
-	else {
-		a->array = temp_array;
-	}
-
-	a->array[a->used++]->firstName = 
 }
 
 void freeArray(Array* a) {
@@ -85,8 +103,11 @@ void freeArray(Array* a) {
 	a->used = a->size = 0;
 }
 
-void addContact() {
+void addContact(Array* a) {
 	printf("Enter a first name: ");
+	fgets(a->array[a->used]->firstName, MAX_LENGTH, stdin);
 	printf("Enter a last name: ");
+	fgets((a->array + a->used)->lastName, MAX_LENGTH, stdin);
 	printf("Enter a phone number: ");
+	fgets((a->array + a->used)->phoneNum, MAX_LENGTH, stdin);
 }
